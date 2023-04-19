80ad98199989adcb272846b3a29a26301063d872
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-04-19 13:59:17 +0700

    08. Loop

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,25 @@
 void main() {
-  try {
-    int umur = int.parse("abc");
-    print(umur);
-  } on FormatException {
-    print("Data yang di input harus angka");
-  } catch (e) {
-    print("Error secara general $e");
+  // for (var i = 1; i <= 100; i++) {
+  //   print(i);
+  // }
+
+  // int i = 1;
+  // do {
+  //   print(i);
+  //   i++;
+  // } while (i <= 10);
+
+  List<String> motor = ["Mio", "Nmax", "Jupiter"];
+
+  // for (var i = 0; i < motor.length; i++) {
+  //   print("Motor : ${motor[i]}");
+  // }
+
+  motor.forEach((element) {
+    print("Motor : $element");
+  });
+
+  for (var element in motor) {
+    print("Motor : $element");
   }
 }
