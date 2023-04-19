eda4082942b91fd06c50d61b567eb43dd4108cd5
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-04-19 17:50:58 +0700

    11. Collections

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,28 +1,57 @@
 void main() {
-  String nama = "KZ";
+  // collection ->variabel yg dapat menyimpan bnyak data
+  // ada 3: List, Set, Map
 
-  // if (nama == "Ahmad") {
-  //   print("Nama dia adalah Ahmad");
-  // } else if (nama == "Ikbal") {
-  //   print("Nama dia adalah Ikbal");
-  // } else if (nama == "Djaya") {
-  //   print("Nama dia adalah Djaya");
-  // } else {
-  //   print("Nama Dia Buka Ahmad, Ikbal, Maupun Djaya, tapi $nama");
+  // List
+  // data pertama index 0 ...
+  // List motor = ["Nmax", "Vario", "Jupiter"];
+  // print(motor[2]);
+
+  // for (var i = 0; i < motor.length; i++) {
+  //   print("Motor ke ${i + 1} : ${motor[i]}");
   // }
 
-  switch (nama) {
-    case "Ahmad":
-      print("Nama dia adalah Ahmad");
-      break;
-    case "Ikbal":
-      print("Nama dia adalah Ikbal");
-      break;
-    case "Djaya":
-      print("Nama dia adalah Djaya");
-      break;
-    default:
-      print("Nama Dia Buka Ahmad, Ikbal, Maupun Djaya, tapi $nama");
-      break;
-  }
+  // int i = 0;
+  // motor.forEach((element) {
+  //   print("Motor ke ${i + 1} : ${element}");
+  //   i++;
+  // });
+
+  // for (var item in motor) {
+  //   print("Motor ke ${i + 1} : ${item}");
+  //   i++;
+  // }
+
+  // Set
+  // index 0...
+  // Set matematikaSet = {8, 9, 7, 6, 10};
+  // Set fisikaSet = {9,8,2,10,5};
+  // print(matematikaSet.elementAt(2));
+  // print(matematikaSet.union(fisikaSet)); //gabung set (angka yg berbeda saja)
+  // print(matematikaSet.intersection(fisikaSet)); //ambil yg sama
+
+  // Map
+  // key : value
+  // Map nilai = {
+  //   "matematika": [8, 9, 7, 6, 10],
+  //   "fisika": [2, 7, 4, 8],
+  // };
+  // print(nilai["matematika"][2]);
+
+  // spreat collection
+  // pada list
+  print("-------------");
+  // kita ingin menambahkan data 5 ke paling akhir -> copyData
+  List data = [8, 9, 7, 6, 10];
+  // penggunaaan spread collection
+  List copyData = [...data, 5];
+  print(copyData[5]);
+
+  // pada set
+  print("-------------");
+  // kita ingin menambahkan data 5 ke paling akhir -> copyData
+  Set dataSet = {8, 9, 7, 6, 10};
+  // penggunaaan spread collection
+  Set copyDataSet = {...data, 5};
+  print(copyDataSet);
 }
