08724bd276f7bd2fb7fa7d9fb30c9faa07c0c5b0
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-04-20 03:15:41 +0700

    12. Null Safety

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,57 +1,44 @@
+// Null Safety -> fitur untuk mengetahui adanya error akibat null
+// versi null safety >= 2.12
+// ada 3 jenis syntax baru:
+// ? ! late
+
+// Kesimpulan
+// 1. ? -> variabel diperbolehkan null
+// 2. ?? -> untuk handle null
+// 3. ! -> sudah pasti tidak null
+// 4. late -> sebelum digunakan wajib di isi / untuk memastikan bahwa sebelum di eksekusi wajib di inisialisasi
+
+// void main() {
+//   String? nama = getNama();
+
+//   print(nama?.length ?? "TIDAK ADA NAMA");
+
+//   if (nama?.length == null) {
+//     // menghandle data null
+//     print("TIDAK ADA DATA NAMA");
+//   } else {
+//     // sudah dipastikan ada datanya
+//     print("$nama terdiri dari ${nama!.length} karakter");
+//   }
+// }
+
+// String? getNama() {
+//   return "Ikbal";
+//   // return null;
+// }
+
+// late
 void main() {
-  // collection ->variabel yg dapat menyimpan bnyak data
-  // ada 3: List, Set, Map
+  late String? nama;
 
-  // List
-  // data pertama index 0 ...
-  // List motor = ["Nmax", "Vario", "Jupiter"];
-  // print(motor[2]);
+  nama = "Djaya";
+  // jika tidak di isi akan otomatis eror di editor
 
-  // for (var i = 0; i < motor.length; i++) {
-  //   print("Motor ke ${i + 1} : ${motor[i]}");
-  // }
+  // proses eksekusi
+  printNama(nama);
+}
 
-  // int i = 0;
-  // motor.forEach((element) {
-  //   print("Motor ke ${i + 1} : ${element}");
-  //   i++;
-  // });
-
-  // for (var item in motor) {
-  //   print("Motor ke ${i + 1} : ${item}");
-  //   i++;
-  // }
-
-  // Set
-  // index 0...
-  // Set matematikaSet = {8, 9, 7, 6, 10};
-  // Set fisikaSet = {9,8,2,10,5};
-  // print(matematikaSet.elementAt(2));
-  // print(matematikaSet.union(fisikaSet)); //gabung set (angka yg berbeda saja)
-  // print(matematikaSet.intersection(fisikaSet)); //ambil yg sama
-
-  // Map
-  // key : value
-  // Map nilai = {
-  //   "matematika": [8, 9, 7, 6, 10],
-  //   "fisika": [2, 7, 4, 8],
-  // };
-  // print(nilai["matematika"][2]);
-
-  // spreat collection
-  // pada list
-  print("-------------");
-  // kita ingin menambahkan data 5 ke paling akhir -> copyData
-  List data = [8, 9, 7, 6, 10];
-  // penggunaaan spread collection
-  List copyData = [...data, 5];
-  print(copyData[5]);
-
-  // pada set
-  print("-------------");
-  // kita ingin menambahkan data 5 ke paling akhir -> copyData
-  Set dataSet = {8, 9, 7, 6, 10};
-  // penggunaaan spread collection
-  Set copyDataSet = {...data, 5};
-  print(copyDataSet);
+void printNama(String? parameterNama) {
+  print(parameterNama);
 }
