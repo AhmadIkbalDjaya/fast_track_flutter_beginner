135ceab18dc3523d5e88cb859fc7fd9ac56817d3
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-04-19 10:51:32 +0700

    05. Comments

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,13 @@
 void main() {
-  // final bisa di isi nanti tapi nilainya tidak bisa di ubah
-  final String name;
-  name = "Ikbal";
+  // satu baris
+  /*
+    multi line
+    multi line
+  */
 
-  // const wajib di isi saat di buat
-  const String addrees = "Home";
-
-  print(name);
-  print(addrees);
-
-// const wajib lansung di isi
-// final boleh nanti
-
-// keduanya tidak diperbolehkan di ubah nilainya
+  /// komen dokumentasi
+  /**
+   * komen dokumentasi multi baris
+   * komen dokumentasi multi baris
+   */
 }
