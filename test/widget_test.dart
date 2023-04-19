676faf0d20568f4318ead029ef2f78cf2dfbea25
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-04-19 10:40:36 +0700

    04. Final & Const

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,45 +1,16 @@
 void main() {
-  // string
-  String name = "IKBAL djaya";
+  // final bisa di isi nanti tapi nilainya tidak bisa di ubah
+  final String name;
+  name = "Ikbal";
 
-  // print(name.toUpperCase());
-  // print(name.substring(6));
-  // print(name.substring(6, 9));
+  // const wajib di isi saat di buat
+  const String addrees = "Home";
 
-  // bool
-  bool isMale = true;
+  print(name);
+  print(addrees);
 
-  print("$name adalah laki laki ? $isMale");
+// const wajib lansung di isi
+// final boleh nanti
 
-  // num, int, double
-  // num bisa bilangan bulan atau desimal
-  // num age = 20;
-  // num age = 20.5;
-  //  int hanya bisa bilangan bulat
-  // int age = 20;
-  // double bisa bilangan desiman dan bilangan bulat (bilangan bulat akan di tambah .0)
-  double age = -47.5;
-
-  // print("Dia berumur  $age Tahun");
-  // print("Dia berumur  ${age.abs()} Tahun");
-  // print("Dia berumur  ${age.ceil()} Tahun");
-  // print("Dia berumur  ${age.abs().ceil()} Tahun");
-  // print("Dia berumur  ${age.abs().floor()} Tahun");
-
-  // list
-  List<String> favColor = ["red", "yellow", "green"];
-  // print("Warna Favorite: $favColor");
-  // print("Warna Favorite: ${favColor[2]}");
-
-  // Map
-  // has key and value
-  Map kendarann = {
-    "mobil": "Lamborgini",
-    "motor": "R1",
-  };
-  print("kendarann: $kendarann");
-  print("Mobil :  ${kendarann['mobil']}");
-
-  // Dynamic
-  // isi list boleh tipe data apapun
+// keduanya tidak diperbolehkan di ubah nilainya
 }
