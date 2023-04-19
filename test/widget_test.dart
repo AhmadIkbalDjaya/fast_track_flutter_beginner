960a4ac1d5cefde022389e87342507d3d31c78cc
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-04-19 13:47:35 +0700

    08. Exceptions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,27 +1,10 @@
-// jalankan pada dartpat.dev
-
-// 3 kondisi
-// uncomplete = menuggu
-// complete ->sukses -> return data
-// comlete -> error ->throw err
-
 void main() {
-  print("A");
-  cetakB().then((value) => print("Sukses")).catchError((err)=> print("Eroro"));
-  print("C");
-}
-
-// void cetakB() {
-//   Future.delayed(Duration(seconds: 2), () {
-//     print("B");
-//   });
-//   print("Berhasil Cetak B");
-// }
-
-Future<String> cetakB() async {
-  await Future.delayed(Duration(seconds: 2));
-  // perintah di bawah tidak akan dijalankan jika await belum selesai
-  print("B");
-  return "Berhasil Cetak B"; //jika sukses
-  // throw "Tidak Berhasil Cetak B"; //jika gagal
+  try {
+    int umur = int.parse("abc");
+    print(umur);
+  } on FormatException {
+    print("Data yang di input harus angka");
+  } catch (e) {
+    print("Error secara general $e");
+  }
 }
