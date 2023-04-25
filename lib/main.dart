5b9b025129fbf9a6c4153b6fc7913b42343acfc4
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-04-25 15:05:45 +0700

    18. Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,20 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
 
-// komen dokumentasi
-/// ini mengarah ke [MyHomePage]
-/// ini juga mengarah ke [MyHomePage]
-
-/**
- * ini ke [MyHomePage]
- * ini ke [MyHomePage]
- */
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        backgroundColor: Color(0xFFD5DDAA),
+        // backgroundColor: Colors.amber, // contoh penguunaan enum
+        body: Center(child: Text("Hello World")),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
