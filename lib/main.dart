2202a44ffda0f328241c778cfeea17933e052677
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-07-08 16:41:39 +0700

    25. Stateles vs Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,58 @@
-import 'package:fast_track_flutter_beginner/latihan/soal_20.dart';
-import 'package:fast_track_flutter_beginner/latihan/soal_21.dart';
-import 'package:fast_track_flutter_beginner/latihan/soal_22.dart';
-import 'package:fast_track_flutter_beginner/latihan/soal_23.dart';
-import 'package:fast_track_flutter_beginner/latihan/soal_24.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  int nilai = 0;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Soal24(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Statefull"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$nilai",
+                style: TextStyle(
+                  fontSize: 50,
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      nilai--;
+                      setState(() {});
+                    },
+                    child: Icon(Icons.remove),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      nilai++;
+                      setState(() {});
+                    },
+                    child: Icon(Icons.add),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
