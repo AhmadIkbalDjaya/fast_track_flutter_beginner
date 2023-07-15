56e3b901f1a4d6dfa20f20cf7bbf0f73155ff179
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-07-15 23:35:24 +0700

    28.TextField

--- lib/main.dart
+++ lib/main.dart
@@ -8,40 +8,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isHidden = true;
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Text Field"),
+      ),
       body: ListView(
         padding: EdgeInsets.all(20),
         children: [
           TextField(
+            controller: emailController,
             autocorrect: false,
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
             decoration: InputDecoration(
               contentPadding:
-                  EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                  EdgeInsets.symmetric(horizontal: 30, vertical: 20),
               label: Text("Email"),
               border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              prefixIcon: Icon(Icons.email),
+            ),
+          ),
+          SizedBox(height: 20),
+          TextField(
+            controller: passwordController,
+            autocorrect: false,
+            obscureText: isHidden,
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              // label: Text("Password"),
+              labelText: "Password",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              prefixIcon: Icon(Icons.key),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    isHidden = !isHidden;
+                  });
+                },
+                icon: isHidden
+                    ? Icon(Icons.remove_red_eye)
+                    : Icon(Icons.remove_red_eye_outlined),
               ),
             ),
           ),
-          TextField(
-            autocorrect: false,
-            obscureText: true,
-            decoration: InputDecoration(
-              label: Text("Password"),
+          SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: () {
+              print(
+                  "Login dengan Email: ${emailController.text} dan pass ${passwordController.text}");
+                  
+            },
+            child: Text("LOGIN"),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
