cd36f7a0b2e4772be8db7aaa74cefb08f5738786
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-10-10 22:19:09 +0800

    31. Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
@@ -15,68 +15,62 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final List data = [
-    {
-      "judul": "Pilihan Ke-1",
-      "data": 1,
-    },
-    {
-      "judul": "Pilihan Ke-2",
-      "data": 2,
-    },
-    {
-      "judul": "Pilihan Ke-3",
-      "data": 3,
-    },
-    {
-      "judul": "Pilihan Ke-4",
-      "data": 4,
-    },
-    {
-      "judul": "Pilihan Ke-5",
-      "data": 5,
-    },
-  ];
+  late int index;
 
-  late int dataAwal;
+  List showWidget = [
+    const Center(
+      child: Text("Home"),
+    ),
+    const Center(
+      child: Text("Cart"),
+    ),
+    const Center(
+      child: Text("Profil"),
+    ),
+  ];
   @override
   void initState() {
-    // TODO: implement initState
+    index = 0;
     super.initState();
-    dataAwal = data[0]["data"];
   }
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Drop Down"),
+        title: const Text("Buttom Navigation Bar"),
       ),
-      body: Center(
-        child: DropdownButton<int>(
-          value: dataAwal,
-          items: data
-              .map(
-                (e) => DropdownMenuItem(
-                  child: Text("${e['judul']} "),
-                  value: e['data'] as int,
-                ),
-              )
-              .toList(),
-          onChanged: (value) {
-            setState(() {
-              dataAwal = value!;
-            });
-            print(value);
-          },
-        ),
+      body: showWidget[index],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        currentIndex: index,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black26,
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profil",
+          ),
+        ],
       ),
     );
   }
